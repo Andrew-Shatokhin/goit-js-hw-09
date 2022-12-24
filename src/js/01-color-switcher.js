babec92c01b903974e2d8f6f5238a804fc05b4d2
Andrew <shatokhin.a.p@gmail.com>
2022-12-24 18:43:22 +0200

    task 1 part 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+startBtn.addEventListener('click', e => {
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+
+  startBtn.disabled = e.currentTarget.click;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
