e8ed24b49cc95c744720ae3f1dbe82c4b5bc2cd2
Andrew <shatokhin.a.p@gmail.com>
2022-12-24 19:29:03 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,18 +2,22 @@ const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 startBtn.addEventListener('click', e => {
   timerId = setInterval(() => {
     document.body.style.background = getRandomHexColor();
   }, 1000);
 
-  startBtn.disabled = e.currentTarget.click;
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
 });
 
-stopBtn.addEventListener('click', () => {
+stopBtn.addEventListener('click', e => {
   clearInterval(timerId);
-});
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+});
