532d7740f8d72be305be9658a82c1f9a77e05757
Andrew-Shatokhin <110032255+Andrew-Shatokhin@users.noreply.github.com>
2022-12-24 17:29:31 +0000

    Deploying to gh-pages from @ Andrew-Shatokhin/goit-js-hw-09@e8ed24b49cc95c744720ae3f1dbe82c4b5bc2cd2 🚀

--- 01-color-switcher.2f2f1134.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");t.addEventListener("click",(function(e){timerId=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=e.currentTarget.click})),e.addEventListener("click",(function(){clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.2f2f1134.js.map
---
+++ 01-color-switcher.8ca781b9.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");t.addEventListener("click",(function(d){timerId=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(d){clearInterval(timerId),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.8ca781b9.js.map
