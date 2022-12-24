db6cfe2beff6e1257b860a567babf2a293d2fea7
Andrew-Shatokhin <110032255+Andrew-Shatokhin@users.noreply.github.com>
2022-12-24 16:43:45 +0000

    Deploying to gh-pages from @ Andrew-Shatokhin/goit-js-hw-09@babec92c01b903974e2d8f6f5238a804fc05b4d2 🚀

---
+++ 01-color-switcher.2e6ed14f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");t.addEventListener("click",(e=>{timerId=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=e.currentTarget.click})),e.addEventListener("click",(()=>{clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.2e6ed14f.js.map
