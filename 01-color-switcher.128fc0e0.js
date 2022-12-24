6dcb989d9001e13d57470630d2d0ce7da5771fc6
Andrew-Shatokhin <110032255+Andrew-Shatokhin@users.noreply.github.com>
2022-12-24 18:55:38 +0000

    Deploying to gh-pages from @ Andrew-Shatokhin/goit-js-hw-09@719a0085bc507e640fdefabefaa5174e1981c6f9 🚀

---
+++ 01-color-switcher.128fc0e0.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");var n=null;t.addEventListener("click",(function(d){n=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(d){clearInterval(n),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.128fc0e0.js.map
